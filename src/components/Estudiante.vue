d318b0fa746c3b208ad07d8c60b1357630ad1703
AndBenvds404 <andres_benavides17@hotmail.com>
2024-02-14 20:53:08 -0500

    taller34

---
+++ src/components/Estudiante.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>Componente Estudiante</h1>
+    <input v-model="id" type="text">
+    <button @click="consultarPorId()">Consultar</button>
+    <input v-model="nombre" type="text">
+
+    <div>
+        <button @click="insertar()">Guardar</button>
+    <input v-model="apellido" type="text">
+    </div>
+    
+
+  
+  
+
+  </div>
+  
+
+</template>
+
+<script>
+import { consultarEstudianteFachada } from '@/helpers/clienteEstudiante'
+import {insertarFachada} from '@/helpers/clienteEstudiante'
+
+//import {consultarEstudiante} from '../helpers/clienteEstudiante'
+
+export default {
+
+    data(){
+        return{
+            id:null,
+            nombre:null,
+            apellido:null
+}
+        
+    },
+    methods:{
+        async consultarPorId(){
+            const data = await consultarEstudianteFachada(this.id);
+            console.log(data);
+            this.nombre = data.nombre
+
+        },
+
+        async insertar(){
+            const estuBody = {
+            
+                nombre: "Marlon",
+                apellido: this.apellido,
+                genero: "f",
+                fechaNacimiento: "2023-10-10T00:00:00"
+            };
+            console.log('Se inserto'+estuBody);
+            await insertarFachada(estuBody);
+        },
+
+        
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/EstudiantePagina.vue
@@ -0,0 +1,22 @@
+<template>
+  
+  <div>
+        <h1>Pagina Estudiante</h1>
+        <Estudiate/>
+  </div>
+</template>
+
+<script>
+
+import Estudiate from '../components/Estudiante.vue'
+export default {
+    components:{
+        Estudiate
+    }
+
+}
+</script>
+
+<style>
+
+</style>
